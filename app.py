2d1692b8282d066d8b8619eb02b25868cfad20fd
Poushali Bhattacharyya <poushalibhattacharyya02@gmail.com>
2025-10-18 19:31:27 +0530

    threshold fixed

--- app.py
+++ app.py
@@ -186,7 +186,7 @@ def start_game():
         pred2 = prediction(preprocess(scenario2))
 
         # Ensure predictions differ by at least 0.5 (adjust threshold as needed)
-        if abs(pred1 - pred2) > 0.5:
+        if abs(pred1 - pred2) > 0.05:
             break
     
     # Store in session state
